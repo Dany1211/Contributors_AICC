fb934d672a2f1e966841d32dff035d1c6c134cca
Dany <dnyanesh2442@gmail.com>
2025-08-06 15:40:32 +0530

    Github repo link updated

--- src/data/contributors.ts
+++ src/data/contributors.ts
@@ -8,14 +8,12 @@ export interface Contributor {
 }
 
 export const contributors: Contributor[] = [
-  // Add your contributor data here
-  // Example:
-  // {
-  //   id: 'your-username',
-  //   name: 'Your Name',
-  //   role: 'Your Role',
-  //   avatar: 'https://images.pexels.com/photos/your-image.jpg',
-  //   github: 'your-username',
-  //   contributions: 1
-  // }
+  {
+    id: 'your-username',
+    name: 'Your Name',
+    role: 'Your Role',
+    avatar: 'https://images.pexels.com/photos/your-image.jpg',
+    github: 'your-username',
+    contributions: 1
+  }
 ];
