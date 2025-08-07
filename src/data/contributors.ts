257da62867d95541cff820f61e1cbc9115c642a0
Dany <dnyanesh2442@gmail.com>
2025-08-07 15:22:49 +0530

    files structure changes

--- src/data/contributors.ts
+++ src/data/contributors.ts
@@ -1,3 +1,5 @@
+import post from '../gallery/post.png'
+
 export interface Contributor {
   id: string;
   name: string;
@@ -13,6 +15,14 @@ export const contributors: Contributor[] = [
     name: 'Shahrukh Khan',
     role: 'Baazigar',
     avatar: 'https://imgs.search.brave.com/SdUK9zJNrcUSdjsp3X_f1dCrsri9nlgW8-TVxfCPbqk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS50ZW5vci5jb20v/RDNvdEt5NDZBV2NB/QUFBTS9oYWtsYS1o/YWtsYS1zaGFocnVr/aC5naWY.gif',
+    github: 'Shahrukh2025',
+    contributions: 1
+  },
+  {
+    id: '2',
+    name: 'Shahrukh Khan',
+    role: 'Baazigar',
+    avatar: post ,
     github: 'Shahrukh2025',
     contributions: 1
   },
